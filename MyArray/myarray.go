e8c6dda130f682541e5acf0cc1636bd2dc31b034
fanfan <piaolongfan@xiimoon.com>
2018-05-23 21:01:20 +0800

    update

--- MyArray/myarray.go
+++ MyArray/myarray.go
@@ -2,10 +2,50 @@ package main
 
 import(
 	util "util/array"
+	"strconv"
+	"fmt"
+	"time"
 )
 
 func main() {
-	array := util.MyArray(20)
+	// myArrayInt()
+	// myArrayString()
+	// myArrayStudent()
+	myArrayTest()
+
+	/*
+	result:
+		RemoveLast() : O(1)
+		RemoveFirst() : O(n)
+		AddLast() : O(1)
+		AddFirst() : O(n)
+
+		Last 和 First 相差 700倍
+	*/
+}
+
+func myArrayTest() {
+	start := time.Now()
+	// myarray ---
+	array := util.MyArray(10)
+	cnt := 10000
+	for i := 0; i < cnt; i++ {
+		array.AddLast(i)
+		// array.AddFirst(i)
+	}
+
+	for i := 0; i < cnt; i++ {
+		array.RemoveLast()
+		// array.RemoveFirst()
+	}
+	array.ToString()
+	// -----------
+	useTime := time.Since(start)
+	fmt.Println("Use time : ", useTime)
+}
+
+func myArrayInt() {
+	array := util.MyArray(2)
 
 	for i := 0; i < 10; i++ {
 		array.AddLast(i)
@@ -25,5 +65,41 @@ func main() {
 	array.ToString()
 
 	array.RemoveFirst()
+	array.ToString()
+}
+
+func myArrayString() {
+	array := util.MyArray(20)
+
+	for i := 0; i < 10; i++ {
+		array.AddLast(strconv.Itoa(i))
+	}
+	array.ToString()
+
+	array.Add(1, "100")
+	array.ToString()
+
+	array.AddFirst("-1")
+	array.ToString()
+
+	array.Remove(2)
+	array.ToString()
+
+	array.RemoveElement("4")
+	array.ToString()
+
+	array.RemoveFirst()
+	array.ToString()
+}
+
+type Student struct{
+	name string
+	score int
+}
+func myArrayStudent() {
+	array := util.MyArray(20)
+	array.AddLast(Student{name:"student1",score:100})
+	array.AddLast(Student{name:"student2",score:80})
+	array.AddLast(Student{name:"student3",score:70})
 	array.ToString()
 }
--- util/array/array.go
+++ util/array/array.go
@@ -5,12 +5,12 @@ import(
 )
 
 type myArray struct {
-	data []int
+	data []interface{}
 }
 
 func MyArray(cap int) *myArray {
 	return &myArray{
-		data: make([]int, 0, cap),
+		data: make([]interface{}, 0, cap),
 	}
 }
 
@@ -26,43 +26,44 @@ func (this *myArray) IsEmpty() bool {
 	return len(this.data) == 0
 }
 
-func (this *myArray) AddFirst(e int) {
+func (this *myArray) AddFirst(e interface{}) {
 	this.Add(0, e)
 }
 
-func (this *myArray) AddLast(e int) {
+func (this *myArray) AddLast(e interface{}) {
 	this.Add(len(this.data), e)
 }
 
-func (this *myArray) Add(index int, e int) {
-	if len(this.data) == cap(this.data) {
-		panic("Add failed. Array is full")
-	}
-	
+func (this *myArray) Add(index int, e interface{}) {
 	if index < 0 || index > len(this.data) {
 		panic("Add failed. Require index >= 0 and index <= size.")
 	}
+
+	if len(this.data) == cap(this.data) {
+		// panic("Add failed. Array is full")
+		this.resize(2 * cap(this.data))
+	}
 	
 	// 保存 slice 中 index 后面的元素
-	slice :=  append([]int{}, this.data[index:]...)
+	slice := append([]interface{}{}, this.data[index:]...)
 	this.data = append(append(this.data[:index], e), slice...)
 }
 
-func (this *myArray) Set(index int, e int) {
+func (this *myArray) Set(index int, e interface{}) {
 	if index < 0 || index >= len(this.data) {
 		panic("Get failed. Index is illegal.")
 	}
 	this.data[index] = e
 }
 
-func (this *myArray) Get(index int) int {
+func (this *myArray) Get(index int) interface{} {
 	if index < 0 || index >= len(this.data) {
 		panic("Get failed. Index is illegal.")
 	}
 	return this.data[index]
 }
 
-func (this *myArray) Contains(e int ) bool {
+func (this *myArray) Contains(e interface{} ) bool {
 	for _, v := range this.data {
 		if v == e {
 			return true
@@ -71,7 +72,7 @@ func (this *myArray) Contains(e int ) bool {
 	return false
 }
 
-func (this *myArray) Find(e int) int {
+func (this *myArray) Find(e interface{}) int {
 	for i, v := range this.data {
 		if v == e {
 			return i
@@ -80,29 +81,40 @@ func (this *myArray) Find(e int) int {
 	return -1
 }
 
-func (this *myArray) Remove(index int) int {
+func (this *myArray) Remove(index int) interface{} {
 	if index < 0 || index >= len(this.data) {
 		panic("Remove failed. Index is illegal")
 	}
 
 	ret := this.data[index]
 	this.data = append(this.data[:index], this.data[index+1:]...)
+
+	if (len(this.data) == cap(this.data) / 4 && len(this.data) / 2 != 0) {
+		this.resize(cap(this.data) / 2)
+	}
+
 	return ret
 }
 
-func (this *myArray) RemoveFirst() int {
+func (this *myArray) RemoveFirst() interface{} {
 	return this.Remove(0)
 }
 
-func (this *myArray) RemoveLast() int {
+func (this *myArray) RemoveLast() interface{} {
 	return this.Remove(len(this.data) - 1)
 }
 
-func (this *myArray) RemoveElement(e int) {
+func (this *myArray) RemoveElement(e interface{}) {
 	index := this.Find(e)
 	if index != -1 {
 		this.Remove(index)
 	}
+}
+
+func (this *myArray) resize(newCapacity int) {
+	slice := this.data
+	this.data = make([]interface{}, 0, newCapacity)
+	this.data = append(this.data, slice...)
 }
 
 func (this *myArray) ToString() {
