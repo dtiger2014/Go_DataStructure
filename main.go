e71fcdef3ad199b62ab95392ee3b3c9c88c883ae
echoFanFan <dtiger2014@163.com>
2019-06-11 00:39:41 +0800

    add myarray

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang-datastruct/util/myarray"
+)
 
 func main() {
-	fmt.Println("Hello,")
+	// TestMyArray()
+
+	// var v interface{}
+	// v = 1
+	// switch t := v.(type) {
+	// case int:
+	// 	fmt.Println("int")
+	// case float64:
+	// 	fmt.Println("float64")
+	// //... etc
+	// default:
+	// 	_ = t
+	// 	fmt.Println("unkown")
+	// }
+}
+
+func TestMyArray() {
+	arr := myarray.New()
+
+	arr.data = make([]int, 4, 4)
+
+	for i := 0; i < 10; i++ {
+		arr.AddLast(i)
+	}
+
+	fmt.Println(arr)
+
+	arr.Add(1, 100)
+	fmt.Println(arr)
+
+	arr.AddFirst("a")
+	fmt.Println(arr)
+
+	arr.Remove(2)
+	fmt.Println(arr)
+
+	arr.RemoveElement(4)
+	fmt.Println(arr)
+
+	arr.RemoveFirst()
+	fmt.Println(arr)
+
+	for i := 0; i < 7; i++ {
+		arr.RemoveFirst()
+		fmt.Println(arr)
+	}
 }
--- util/myarray/myarray.go
+++ util/myarray/myarray.go
@@ -1,5 +1,10 @@
 package myarray
 
+import (
+	"errors"
+	"fmt"
+)
+
 const (
 	// DefaultCap : 默认数组容量
 	DefaultCap = 10
@@ -7,15 +12,19 @@ const (
 
 // ArrayData : 动态数组 struct
 type ArrayData struct {
+	data []interface{}
 	size int
-	data [DefaultCap]interface{}
 }
 
-func (arr *ArrayData) Init() {
-	arr.size = 0
+func New() *ArrayData {
+	return &ArrayData{
+		data: make([]interface{}, DefaultCap, DefaultCap),
+		size: 0,
+	}
 }
+
 func (arr *ArrayData) GetCapacity() int {
-	return len(arr.data)
+	return cap(arr.data)
 }
 
 func (arr *ArrayData) GetSize() int {
@@ -26,20 +35,123 @@ func (arr *ArrayData) IsEmpty() bool {
 	return arr.size == 0
 }
 
-func (arr *ArrayData) Add(index int, E interface{}) error {
+func (arr *ArrayData) Add(index int, e interface{}) error {
 	if index < 0 || index > arr.size {
-		// return error.New("")
-		return nil
+		return errors.New("Add failed. Require index >= 0 and index <= size.")
 	}
 
-	// if size == len(arr.data){
-	// 	arr.resize()
-	// }
+	if arr.size == cap(arr.data) {
+		arr.resize(2 * cap(arr.data))
+	}
+
+	for i := arr.size - 1; i >= index; i-- {
+		arr.data[i+1] = arr.data[i]
+	}
+
+	arr.data[index] = e
+	arr.size++
+
+	return nil
+}
+
+func (arr *ArrayData) AddLast(e interface{}) {
+	arr.Add(arr.size, e)
+}
+
+func (arr *ArrayData) AddFirst(e interface{}) {
+	arr.Add(0, e)
+}
+
+func (arr *ArrayData) Get(index int) (interface{}, error) {
+	if index < 0 || index > arr.size {
+		return nil, errors.New("Get failed. Index is illegal.")
+	}
+
+	return arr.data[index], nil
+}
+
+func (arr *ArrayData) Set(index int, e interface{}) error {
+	if index < 0 || index > arr.size {
+		return errors.New("Set failed. Index is illegal.")
+	}
+
+	arr.data[index] = e
+
+	return nil
+}
+
+func (arr *ArrayData) Contains(e interface{}) bool {
+	for i := 0; i < arr.size; i++ {
+		if arr.data[i] == e {
+			return true
+		}
+	}
+	return false
+}
+
+func (arr *ArrayData) Find(e interface{}) int {
+	for i := 0; i < arr.size; i++ {
+		if arr.data[i] == e {
+			return i
+		}
+	}
+	return -1
+}
+
+func (arr *ArrayData) Remove(index int) (interface{}, error) {
+	if index < 0 || index >= arr.size {
+		return nil, errors.New("Remove failed. Index is illegal.")
+	}
+
+	ret := arr.data[index]
+
+	for i := index + 1; i < arr.size; i++ {
+		arr.data[i-1] = arr.data[i]
+	}
+	arr.size--
+	arr.data[arr.size] = nil
+
+	if arr.size == cap(arr.data)/4 && cap(arr.data)/2 != 0 {
+		arr.resize(cap(arr.data) / 2)
+	}
+
+	return ret, nil
+}
+
+func (arr *ArrayData) RemoveFirst() (interface{}, error) {
+	return arr.Remove(0)
+}
+
+func (arr *ArrayData) RemoveLast() (interface{}, error) {
+	return arr.Remove(len(arr.data))
+}
+
+func (arr *ArrayData) RemoveElement(e interface{}) {
+	index := arr.Find(e)
+	if index != 1 {
+		_, _ = arr.Remove(index)
+	}
+}
+
+func (arr *ArrayData) String() string {
+	str := fmt.Sprintf("Array: size = %d , capacity = %d\n", arr.size, cap(arr.data))
+	str += "["
+	for i := 0; i < arr.size; i++ {
+		str += fmt.Sprintf("%v", arr.data[i])
+		if i != arr.size-1 {
+			str += ", "
+		}
+	}
+	str += "]"
+
+	return str
 }
 
 func (arr *ArrayData) resize(newCapacity int) {
-	newArr := [newCapacity]interface{}
-	for i, v := range arr.data {
-		newArr[i] = v
+	newArr := make([]interface{}, newCapacity, newCapacity)
+	for i := 0; i < arr.size; i++ {
+		newArr[i] = arr.data[i]
 	}
+
+	arr.data = newArr
 }
